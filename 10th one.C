71cdfd69b7b69ebe796bdaf32f149c28cbcc6f96
si5540 <113980903+si5540@users.noreply.github.com>
2022-10-12 10:58:11 +0530

    Update and rename 10th one to 10th one.C

--- 10th one.C
+++ 10th one.C
@@ -2,11 +2,12 @@
 
 int main()
 {
+   printf("RA2211042010015");
    int num1, num2;
    printf("Enter First Number=");
-   scanf("%d", &num1);
+   scanf("%d", num1);
    printf("Enter 2nd Number=");
-   scanf("%d", &num2);
+   scanf("%d", num2);
    if(num1 == num2)
      printf("First Number %d is equal to Second Number %d ", num1,num2);
    else
